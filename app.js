bbf60927ec407e56ccd4dd8578e76f54e6318f84
primus17 <andressueyro@gmail.com>
2021-07-19 02:17:01 -0300

    agrego localhost

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000
+const path = require("path")
 
+app.listen(port,()=>{console.log("el servidor esta abierto en el puerto n° "+port)})
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve("views/index.html"))
+});
